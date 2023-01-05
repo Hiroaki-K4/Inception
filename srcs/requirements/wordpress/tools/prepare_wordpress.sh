9d2b639cfc4f9cc150adcc6ddcca3a0e619fad32
Hiroaki-K4 <49no19@gmail.com>
2023-01-05 23:14:56 +0900

    Add nginx conf about location

--- srcs/requirements/wordpress/tools/prepare_wordpress.sh
+++ srcs/requirements/wordpress/tools/prepare_wordpress.sh
@@ -2,11 +2,8 @@
 
 # chmod 744 /var/www/wp-config.php
 
-# chown -R www-data:www-data /var/www/html/*
-# chown -R 755 /var/www/html/wordpress/*
-
 mv wp-config.php /var/www/html/wordpress
 
-# chown -R www-data:www-data /var/www/html/*
+chown -R www-data:www-data /var/www/html/*
 
 /usr/sbin/php-fpm7.3 --nodaemonize
