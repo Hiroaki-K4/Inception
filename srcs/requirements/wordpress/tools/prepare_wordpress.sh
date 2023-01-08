8632a693dcec41316c90a61659ac726cc57000bf
Hiroaki-K4 <49no19@gmail.com>
2023-01-08 14:09:23 +0900

    Update volumes setup

--- srcs/requirements/wordpress/tools/prepare_wordpress.sh
+++ srcs/requirements/wordpress/tools/prepare_wordpress.sh
@@ -1,8 +1,9 @@
 # !/bin/sh
 
 # chmod 744 /var/www/wp-config.php
-
+mv wordpress /var/www/html
 mv wp-config.php /var/www/html/wordpress
+
 mv www.conf /etc/php/7.3/fpm/pool.d
 
 chown -R www-data:www-data /var/www/html/*
